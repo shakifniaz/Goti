4b0bca4dd50457a0978ea528762fe3f67d191a9f
ishfak-akbar <ishfakakbar24@gmail.com>
2025-04-29 20:24:59 +0600

    thura size suto boro korsi, mobile screen o fit korar lagi

--- app/src/main/java/com/example/goti/DriverRegistrationActivity.java
+++ app/src/main/java/com/example/goti/DriverRegistrationActivity.java
@@ -1,5 +1,6 @@
 package com.example.goti;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
@@ -13,8 +14,11 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Calendar;
+import java.util.Locale;
+
 public class DriverRegistrationActivity extends AppCompatActivity {
-    private EditText mEmail, mPassword;
+    private EditText mEmail, mPassword,mDob;
     private Button mRegister;
     private FirebaseAuth mAuth;
 
@@ -28,6 +32,24 @@ public class DriverRegistrationActivity extends AppCompatActivity {
         mEmail = findViewById(R.id.email);
         mPassword = findViewById(R.id.password);
         mRegister = findViewById(R.id.registerButton);
+
+        mDob = findViewById(R.id.dob);
+        mDob.setOnClickListener(v -> {
+            final Calendar calendar = Calendar.getInstance();
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(
+                    DriverRegistrationActivity.this,
+                    (view, year1, monthOfYear, dayOfMonth) -> {
+                        String selectedDate = String.format(Locale.getDefault(), "%02d/%02d/%04d", dayOfMonth, monthOfYear + 1, year1);
+                        mDob.setText(selectedDate);
+                    },
+                    year, month, day);
+
+            datePickerDialog.show();
+        });
 
         mRegister.setOnClickListener(v -> {
             String email = mEmail.getText().toString();
