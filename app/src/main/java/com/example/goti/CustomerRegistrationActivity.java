81d9061749e9c820ac84930d41d5c82ddf364b98
ishfak-akbar <ishfakakbar24@gmail.com>
2025-04-29 18:00:37 +0600

    border white

--- app/src/main/java/com/example/goti/CustomerRegistrationActivity.java
+++ app/src/main/java/com/example/goti/CustomerRegistrationActivity.java
@@ -1,5 +1,6 @@
 package com.example.goti;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
@@ -13,8 +14,11 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Calendar;
+import java.util.Locale;
+
 public class CustomerRegistrationActivity extends AppCompatActivity {
-    private EditText mEmail, mPassword;
+    private EditText mEmail, mPassword, mDob;
     private Button mRegister;
     private FirebaseAuth mAuth;
 
@@ -29,6 +33,23 @@ public class CustomerRegistrationActivity extends AppCompatActivity {
         mPassword = findViewById(R.id.password);
         mRegister = findViewById(R.id.registerButton);
 
+        mDob = findViewById(R.id.dob);
+        mDob.setOnClickListener(v -> {
+            final Calendar calendar = Calendar.getInstance();
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(
+                    CustomerRegistrationActivity.this,
+                    (view, year1, monthOfYear, dayOfMonth) -> {
+                        String selectedDate = String.format(Locale.getDefault(), "%02d/%02d/%04d", dayOfMonth, monthOfYear + 1, year1);
+                        mDob.setText(selectedDate);
+                    },
+                    year, month, day);
+
+            datePickerDialog.show();
+        });
         mRegister.setOnClickListener(v -> {
             String email = mEmail.getText().toString();
             String password = mPassword.getText().toString();
